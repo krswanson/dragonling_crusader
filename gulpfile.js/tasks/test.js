ae7f979727b04662c79df4cee8c1a07b2fe0be18
Kristin Swanson <kristin@trackif.com>
2017-11-30 14:00:54 -0800

    Adding gulp-mocha

---
+++ gulpfile.js/tasks/test.js
@@ -0,0 +1,28 @@
+import gulp from 'gulp'
+import standard from 'gulp-standard'
+import mocha from 'gulp-mocha'
+import util from 'gulp-util'
+
+function lint () {
+  return gulp.src(['app.js', 'src/**/*.js', 'gulpfile.js/**/*.js'])
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: false,
+      quiet: true
+    }))
+}
+
+function unit () {
+  return gulp.src(['test/**/*.js'], { read: false })
+    .pipe(mocha({
+      reporter: 'nyan',
+      require: ['babel-core/register']
+    }))
+    .on('error', util.log)
+}
+
+const test = gulp.series([unit, lint])
+
+gulp.task('test', test)
+
+module.exports = test
---
+++ test/dragon_test.js
@@ -0,0 +1,5 @@
+import dragon from '../src/dragon.js'
+
+describe('Dragon', () => {
+  it('does something', () => {})
+})
