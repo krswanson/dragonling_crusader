1c7fdbaeb409f807c97ded74a8463c464a8ddb19
Kristin Swanson <kristin@trackif.com>
2017-12-06 14:35:47 -0800

    Adding LESS and background color

--- gulpfile.js/tasks/build.js
+++ gulpfile.js/tasks/build.js
@@ -3,8 +3,23 @@ import browserify from 'browserify'
 import through2 from 'through2'
 import fs from 'fs'
 import path from 'path'
+import less from 'less'
 
-function build () {
+function lessToCss (callback) {
+  fs.unlink(path.resolve('dist/index.css'), function (err) { if (err) console.warn('No dist/index.css file found: creating it...') })
+  fs.readFile(path.resolve('index.less'), 'utf8', function (err, data) {
+    if (err) throw err
+    less.render(data, function (err, output) {
+      if (err) throw err
+      fs.writeFile('dist/index.css', output.css, function (err) {
+        if (err) throw err
+        return callback()
+      })
+    })
+  })
+}
+
+function browserifyJS () {
   fs.unlink(path.resolve('dist/index.js'), function (err) { if (err) console.warn('No dist/index.js file found: creating it...') })
   return gulp.src(['./src/index.js'])
     .pipe(through2.obj(function (file, enc, next) {
@@ -18,6 +33,7 @@ function build () {
     }))
     .pipe(gulp.dest('./dist/'))
 }
+let build = gulp.series([browserifyJS, gulp.series(lessToCss)])
 gulp.task('build', build)
 
 module.exports = build
--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -3,7 +3,7 @@ import build from './build'
 import test from './test'
 
 function watch () {
-  gulp.watch(['index.html', 'index.css', 'app.js', 'src/**/*.js', 'images/**/*.png', 'test/**/*.js'], gulp.series(test, build))
+  gulp.watch(['index.html', 'index.less', 'app.js', 'src/**/*.js', 'images/**/*.png', 'test/**/*.js'], gulp.series(test, build))
 }
 
 gulp.task('watch', gulp.series(test, build, watch))
