83188f279d103fd87d0b700d0feeab45441d3428
Kristin Swanson <kristin@trackif.com>
2017-12-15 15:45:40 -0800

    Better usability of level select

--- src/index.js
+++ src/index.js
@@ -7,30 +7,26 @@ let currentLevel = 0
 let levelKeys = Object.keys(levels)
 
 function setSelect (levelKey) {
-  console.log('levelKey', levelKey)
-  $('option[value=' + levelKey + ']', document)
+  $('#level-select')[0].value = levelKey
+  $('option[value="' + levelKey + '"]', document)
   .attr('selected', true).siblings()
   .removeAttr('selected')
-  $('')
 }
 
-$(document).on('change', 'select', function () {
-  setSelect(this.value)
-})
+let closed = true
 
 function inputClick (input) {
+  closed = !closed
   $(input).closest('div').find('select').slideToggle(110)
 }
 
 function updateSelectText (element) {
-  console.log($(element))
   $(element).hide().closest('div').find('input').val($(element).find('option:selected').text())
 }
 
 function winGame () {
   let message = $('#level-description')[0]
-  message.innerHTML = 'Your dragonlings have conquered!<br>You beat the game!'
-  message.style.color = '#11ee11'
+  message.innerHTML = '<p style="color: #11ee11">Your dragonlings have conquered!<br>You beat the game!</p>'
   // TODO place with all colors declared
 }
 
@@ -46,7 +42,7 @@ let selectLevel = function (key) {
       return console.error('Problem loading level. Bad key:', givenKey)
     }
   }
-  setSelect(key)
+  $('#level-number')[0].innerHTML = key
   board.setup(level)
   currentLevel = levelKeys.indexOf(key)
 }
@@ -54,12 +50,15 @@ let selectLevel = function (key) {
 levelKeys.forEach(function (key, i) {
   let option = document.createElement('OPTION')
   option.value = key
-  option.innerHTML = 'Level ' + (i + 1)
+  option.innerHTML = key
   let select = document.getElementById('level-select')
   if (i === 0) select.value = key
   select.appendChild(option)
 })
 
+$(document).on('change', 'select', function () {
+  setSelect(this.value)
+})
 document.getElementById('restart-level').addEventListener('click', function () {
   selectLevel(currentLevel)
 })
@@ -71,11 +70,54 @@ document.getElementById('next-level').addEventListener('click', function () {
 document.getElementById('level-select').addEventListener('click', function () {
   updateSelectText(this)
 })
-document.getElementById('level-select-button').addEventListener('click', function () {
-  selectLevel($('#level-select')[0].value)
+document.getElementById('level-select-button')
+.addEventListener('click', function () {
+  let input = $('#previous-level-input')[0].value || $('#level-select')[0].value
+  if (levelKeys.indexOf(input) >= 0) selectLevel(input)
+  else {
+    $('#previous-level-input').val('')
+    // TOTDO error message
+  }
 })
-document.getElementById('previous-level-input').addEventListener('click', function () {
+let prevLevelInput = document.getElementById('previous-level-input')
+prevLevelInput.addEventListener('click', function () {
   inputClick(this)
+})
+// TODO disable dragon moving while editing input field
+prevLevelInput.addEventListener('keyup', function(event) {
+    event.preventDefault()
+    console.log('event', event.which)
+    let oldText = $('#level-select').find('option:selected').val()
+    let key = null
+    let newText = null
+    switch (event.which) {
+      case 13:
+        if (!closed) inputClick(this)
+        newText = $('#previous-level-input').val()
+        console.log('vals', newText, levelKeys.indexOf(newText))
+        if (levelKeys.indexOf(newText) >= 0) setSelect(newText)
+        document.getElementById('level-select-button').click()
+        break;
+      case 37:
+      case 38:
+        key = oldText ? levelKeys.indexOf(oldText) - 1 : 0
+        if (key < 0) key = 0
+        newText = levelKeys[key]
+        setSelect(newText)
+        $('#previous-level-input').val(newText)
+        break;
+      case 39:
+      case 40:
+        oldText = $('#level-select').find('option:selected').val()
+        key = oldText ? levelKeys.indexOf(oldText) + 1 : 0
+        if (key >= levelKeys.length) key = levelKeys.length - 1
+        newText = levelKeys[key]
+        setSelect(newText)
+        $('#previous-level-input').val(newText)
+        break;
+      default:
+        // Nothing
+    }
 })
 
 selectLevel(currentLevel)
--- src/levelSetup.js
+++ src/levelSetup.js
@@ -9,57 +9,44 @@ let dirt = '#995533'
 let ice = '#aaccff'
 let wizardry = '#eeee33'
 
-let fireLevel = new Level()
-fireLevel.setMapColors(4, 4, grass, 'orange')
-fireLevel.addKnight(2, 2)
-fireLevel.addDragon('Fire Dragonling', 'fire', 'orange',
+let lv = new Level()
+lv.setMapColors(4, 4, grass, 'orange')
+lv.addKnight(2, 2)
+lv.addDragon('Fire Dragonling', 'fire', 'orange',
   {
     '#44aa11': 'orange',
     'orange': grass
   }
 )
-fireLevel.description = 'Your dragonlings start their conquest on a grassy field.  This lands\' knights seem fond of it for some reason, so obviously you must take it away from them! Send this fire dragonling to take it over.'
-levels['fire_1'] = fireLevel
+lv.description = 'Your dragonlings start their conquest on a grassy field.  This lands\' knights seem fond of it for some reason, so obviously you must take it away from them! Send this fire dragonling to take it over.'
+levels['Level 1'] = lv
 
-let fire2 = new Level(fireLevel)
-fire2.addKnight(2, 2)
-fire2.addDragon('Fire Dragonling', 'fire', 'orange',
+lv = new Level(lv)
+lv.setMapColors(4, 4, wetGrass, 'orange')
+lv.addKnight(2, 2)
+lv.addDragon('Fire Dragonling', 'fire', 'orange',
   {
-    '#44aa11': dirt,
+    '#11aa55': dirt,
     '#995533': 'orange',
     'orange': dirt
   }
 )
-fire2.description = 'That wasn\'t so hard! Have your dragonling set this even grassy-er field on fire. You may need to burn the grass to the ground before it will catch properly.'
-levels['fire_2'] = fire2
+lv.description = 'The enemy is has found a way to deter our progress. Looks like they have thrown water on this field. Might need to sear the grass down to dirt before the fire will take.'
+levels['Level 2'] = lv
 
-let fire3 = new Level()
-fire3.setMapColors(4, 4, wetGrass, 'orange')
-fire3.addKnight(2, 1)
-fire3.addDragon('Fire Dragonling', 'fire', 'orange',
-  {
-    '#11aa55': dirt,
-    '#995533': 'red',
-    'red': 'orange',
-    'orange': dirt
-  }
-)
-fire3.description = 'The enemy is getting smarter. Looks like they have thrown water on this field. You may need to heat the ground to get all the water out. But you must take it to get to the castle!'
-levels['fire_3'] = fire3
-
-let iceLevel = new Level()
-iceLevel.setMapColors(4, 4, wetGrass, ice)
-iceLevel.addKnight(1, 1)
-iceLevel.addDragon('Ice Dragonling', 'ice', ice,
+lv = new Level()
+lv.setMapColors(4, 4, wetGrass, ice)
+lv.addKnight(1, 1)
+lv.addDragon('Ice Dragonling', 'ice', ice,
   {
     '#11aa55': ice,
     '#aaccff': wetGrass
   }
 )
-iceLevel.description = 'Aha, new plan! Your ice dragonling reinforcements have arrived. It will be easy to freeze even a water-laden field.'
-levels['ice_1'] = iceLevel
+lv.description = 'Aha, new plan! Your ice dragonling reinforcements have arrived. It will be easy to freeze even a water-laden field.'
+levels['Level 3'] = lv
 
-let lv = new Level()
+lv = new Level()
 lv.setMapColors(4, 4, wizardry, ice)
 lv.addKnight(1, 1)
 lv.addDragon('Ice Dragonling', 'ice', ice,
@@ -73,7 +60,7 @@ lv.goalColors[2][2] = 'gray'
 lv.baseColors[2][2] = 'gray'
 Object.keys(lv.characters).forEach(k => { lv.characters[k].addInvalidColor('gray') })
 lv.description = 'Hmm, this field\'s grass looks strange.  Did they do something to it?  And what\'s up with the rock?'
-levels['ice_2'] = lv
+levels['Level 4'] = lv
 
 lv = new Level()
 lv.setMapColors(5, 5, wizardry, ice)
@@ -87,7 +74,7 @@ lv.addDragon('Ice Dragonling', 'ice', ice,
   }
 )
 lv.description = 'Two knights now! What are they defending? The castle is still a ways off.'
-levels['ice_3'] = lv
+levels['Level 5'] = lv
 
 lv = new Level()
 lv.setMapColors(5, 5, grass, ice)
@@ -103,6 +90,6 @@ lv.addDragon('Ice Dragonling', 'ice', ice,
 lv.baseColors[0][2] = wizardry
 lv.baseColors[1][2] = wizardry
 lv.description = 'Aha! This strangely-colored grass is a wizard\'s work!'
-levels['ice_4'] = lv
+levels['Level 6'] = lv
 
 module.exports = levels
