ff97b6055326fb4439870a9d2c1e3be4ab8e3e22
krswanson <kristin@swanson.name>
2018-04-04 20:52:59 -0700

    Check for process.env.PORT

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
+const port = process.env.PORT || 4005
 app.get('/', function (req, res) {
   res.sendFile(path.resolve('index.html'))
 })
 
 app.use(express.static(__dirname))
 
-app.listen(4005, function () {
-  console.log('Example app listening on port 4005: http://localhost:4005/index.html')
+app.listen(port, function () {
+  console.log('App listening on port:', port)
 })
